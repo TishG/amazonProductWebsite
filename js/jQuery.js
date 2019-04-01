9e9bdc8eea1e74a94a9ffc7ef1bcd085cea7dbb7
Tish <latisha.griffiths@programmer.net>
2019-04-01 13:23:01 -0500

    fade toggle for gallery image, changed navbar toggler icon and added toggleClass to it

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
   
+    //navbar items highlight on scroll
     $('body').scrollspy({
         target: '#myNavbar',
         offset: 50
       });
 
+    //fade gallery in and out
     $(".btn-secondary").click(function() {
-        $("#gallery-images").toggle();
+        $("#gallery-images").fadeToggle(600);
     })
+
+    //toggle navbar icon
+    $('.navbar-toggler').click(
+      function() {
+          $(this).find("span").toggleClass("fa-angle-down fa-angle-up")
+    });
 
  });
